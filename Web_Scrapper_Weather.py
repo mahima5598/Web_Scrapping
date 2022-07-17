58fd4c5c805a7cf90ecf28ca0e073b96578aae5a
mahima5598 <35658037+mahima5598@users.noreply.github.com>
2022-07-17 11:56:32 -0400

    Update Web_Scrapper_Weather.py

--- Web_Scrapper_Weather.py
+++ Web_Scrapper_Weather.py
@@ -14,29 +14,26 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 while True:
-
 	def tag_visible(element):
-	    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-		return False
-	    if isinstance(element, Comment):
-		return False
-	    return True
-
+		if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+			return False
+		if isinstance(element, Comment):
+			return False
+		return True
+	
 
 	def text_from_html(body):
-	    soup = BeautifulSoup(body, 'html.parser')
-	    texts = soup.findAll(text=True)
-	    visible_texts = filter(tag_visible, texts)  
-	    return u" ".join(t.strip() for t in visible_texts)
+		soup = BeautifulSoup(body, 'html.parser')
+		texts = soup.findAll(text=True)
+		visible_texts = filter(tag_visible, texts)  
+		return u" ".join(t.strip() for t in visible_texts)
 
 	html = urllib.request.urlopen('http://www.solarvu.net/green/solarvu/performWeatherTrak.php?ac=101948&wt=1').read()
 	Weather_data=text_from_html(html)
 	#print(Weather_data)
 
-
 	def convert_to_json(Weather_data):
-		
-		
+		x = 0
 		Weather_raw = Weather_data.split()
 		#print(Weather_raw)
 
@@ -58,74 +55,72 @@ while True:
 		print(Weather_datavalue)
 	#At night most values turn to N/A, the program is not efficient enough to input N/A i.e. string and integer paralelly to InfluxDB
 		Weather_values = {}	
-		while (Weather_datavalue[2] != 0.0)
-			Weather_values = {
-				"Solar_Irradiance": Weather_datavalue[2],
-				"Peak_Solar_Irradiance" : Weather_datavalue[3],
-				"Insolation": Weather_datavalue[4],
-				"Full_Sun_Hours": Weather_datavalue[5],
-				"Ambient_temperature" : Weather_datavalue[6],
-				"Peak_Ambient_Temperature": Weather_datavalue[7],
-				"Panel_temperature" : Weather_datavalue[8],
-				"Peak_Panel_Temperature": Weather_datavalue[9],
-				"Peak_Irradiance" : Weather_datavalue[10],
-				"Max_Insolation": Weather_datavalue[11],
-				"Max_Ambient_temperature" : Weather_datavalue[12],
-				"Min_Ambient_Temperature": Weather_datavalue[13],
-				"Max_Panel_temperature" : Weather_datavalue[14],
-				"Min_Panel_Temperature": Weather_datavalue[15],
-				"DC_Input_Capacity": Weather_datavalue[16],
-				"AC_Output_Capacity" : Weather_datavalue[17]
-				}
+		#while Weather_datavalue[2] != float(x):
+		Weather_values = {	
+			"Solar_Irradiance": Weather_datavalue[2],
+			"Peak_Solar_Irradiance" : Weather_datavalue[3],
+			"Insolation": Weather_datavalue[4],
+			"Full_Sun_Hours": Weather_datavalue[5],
+			"Ambient_temperature" : Weather_datavalue[6],
+			"Peak_Ambient_Temperature": Weather_datavalue[7],
+			"Panel_temperature" : Weather_datavalue[8],
+			"Peak_Panel_Temperature": Weather_datavalue[9],
+			"Peak_Irradiance" : Weather_datavalue[10],
+			"Max_Insolation": Weather_datavalue[13],
+			"Max_Ambient_temperature" : Weather_datavalue[17],
+			"Min_Ambient_Temperature": Weather_datavalue[20],
+			"Max_Panel_temperature" : Weather_datavalue[23],
+			"Min_Panel_Temperature": Weather_datavalue[26],
+			"DC_Input_Capacity": Weather_datavalue[29],
+			"AC_Output_Capacity" : Weather_datavalue[30]
+			}
 		return json.dumps(Weather_values)
 
 	Weather = convert_to_json(Weather_data)
 	Json_Weather = json.loads(Weather)
 
-	print(Json_Weather)
+	#print(Json_Weather)
 	
-	try:
-		# You can generate an API token from the "API Tokens Tab" in the UI
-		token = "UQ9CBHaU8JXRxx8Qwp9YVR_7yjE-kNnju_YtcCwBO27QAsqmnqeJxrU0fnoStTA4iOyiMM9qZv0qH9pjrwP_nA=="
-		org = "Volpow"
-		bucket = "Client_lib_test"
+	# You can generate an API token from the "API Tokens Tab" in the UI
+	token = "_mEZ0zAHEybHXBbq4d8idJE3PWM68K6EG0PXITwC56-6zOAZ-kyl3I4ONzZFpewquEjUzHZkDAs6vrbEviJO7w=="
+	org = "Volpow"
+	bucket = "Webscrapper"
 
-		client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
+	client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
 
-		write_api = client.write_api(write_options=SYNCHRONOUS)
+	write_api = client.write_api(write_options=SYNCHRONOUS)
 
-		p = (
-			influxdb_client.Point("Weather_test3")
-			.tag("SolarVu_Weather","SolarVu_Weather")
-			.field("Solar_Irradiance", Json_Weather["Solar_Irradiance"])
-			.field("Peak_Solar_Irradiance", Json_Weather["Peak_Solar_Irradiance"])
-			.field("Insolation", Json_Weather["Insolation"])
-			.field("Full_Sun_Hours", Json_Weather["Full_Sun_Hours"])
-			.field("Ambient_temperature", Json_Weather["Ambient_temperature"])
-			.field("Peak_Ambient_Temperature", int(Json_Weather	["Peak_Ambient_Temperature"]))
-			.field("Panel_temperature", Json_Weather["Panel_temperature"])
-			.field("Peak_Panel_Temperature", Json_Weather["Peak_Panel_Temperature"])
-			.field("Peak_Irradiance", Json_Weather["Peak_Irradiance"])
-			.field("Max_Insolation", Json_Weather["Max_Insolation"])
-			.field("Max_Ambient_temperature", Json_Weather["Max_Ambient_temperature"])
-			.field("Min_Ambient_Temperature", Json_Weather["Min_Ambient_Temperature"])
-			.field("Max_Panel_temperature", Json_Weather["Max_Panel_temperature"])
-			.field("DC_Input_Capacity", Json_Weather["DC_Input_Capacity"])
-			.field("AC_Output_Capacity", Json_Weather["AC_Output_Capacity"])
-					        
-		)	
+	q = (
+		influxdb_client.Point("Weather")
+		.tag("SolarVu_Weather","SolarVu_Weather")
+		.field("Solar_Irradiance", Json_Weather["Solar_Irradiance"])
+		.field("Peak_Solar_Irradiance", Json_Weather["Peak_Solar_Irradiance"])
+		.field("Insolation", Json_Weather["Insolation"])
+		.field("Full_Sun_Hours", Json_Weather["Full_Sun_Hours"])
+		.field("Ambient_temperature", Json_Weather["Ambient_temperature"])
+		.field("Peak_Ambient_Temperature", Json_Weather	["Peak_Ambient_Temperature"])
+		.field("Panel_temperature", Json_Weather["Panel_temperature"])
+		.field("Peak_Panel_Temperature", Json_Weather["Peak_Panel_Temperature"])
+		.field("Peak_Irradiance", Json_Weather["Peak_Irradiance"])
+		.field("Max_Insolation", Json_Weather["Max_Insolation"])
+		.field("Max_Ambient_temperature", Json_Weather["Max_Ambient_temperature"])
+		.field("Min_Ambient_Temperature", Json_Weather["Min_Ambient_Temperature"])
+		.field("Max_Panel_temperature", Json_Weather["Max_Panel_temperature"])
+		.field("DC_Input_Capacity", Json_Weather["DC_Input_Capacity"])
+		.field("AC_Output_Capacity", Json_Weather["AC_Output_Capacity"])
+				        
+	)	
 	     
-		write_api.write(bucket, org, record=p)
+	write_api.write(bucket, org, record=q)
 
-		query = 'from(bucket: "Client_lib_test") |> range(start: -1h)'
-		tables = client.query_api().query(query, org=org)
+	query = 'from(bucket: "Webscrapper") |> range(start: -1h)'
+	tables = client.query_api().query(query, org=org)
 
-		for table in tables:
-		    for record in table.records:
+	for table in tables:
+		for record in table.records:
 			print(record)
 
-		client.close()
-	except:
-		None
+	client.close()
+	
 	time.sleep(600)
 
